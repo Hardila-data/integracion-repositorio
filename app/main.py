31d27680dc86d26dfc72c5751440e1bafa19973c
Hector Ardila <hectorardialsoto@gmail.com>
2024-09-09 08:37:20 -0500

    Funciones Javascript

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+# Initialize the app
+app = FastAPI()
+# Get the templates
+templates = Jinja2Templates(directory="app/templates")
+#get request to get the HTML template
+@app.get("/contador", response_class=HTMLResponse)
+def contador(request:Request):
+    return templates.TemplateResponse(request=request, name="index.html")
